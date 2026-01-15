f330dbfa6a3945bf34e4ea6199b206afdf98b234
Mike Uroda <itmeine@gmail.com>
2026-01-15 13:16:20 +0100

    deploy: zmiana na postgres i przygotowanie do deploy na railway

--- prisma/migrations/20250115000000_add_settlement_status_fields/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Affair" ADD COLUMN "settlementProposalStatus" TEXT;
-ALTER TABLE "Affair" ADD COLUMN "settlementAcceptedBy" TEXT;
-ALTER TABLE "Affair" ADD COLUMN "settlementModificationRequests" TEXT;
-
--- AlterTable
-ALTER TABLE "AffairParticipant" ADD COLUMN "settlementAcceptedAt" DATETIME;
-ALTER TABLE "AffairParticipant" ADD COLUMN "settlementModificationRequestedAt" DATETIME;
